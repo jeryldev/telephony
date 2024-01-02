b9ed3e1612c18f7f97d117869ad77ac813a3c2ca
jeryldev <jeryldev@gmail.com>
2024-01-02 22:50:17 +0800

    add test to append new subscriber to existing subscribers

--- lib/telephony/core.ex
+++ lib/telephony/core.ex
@@ -2,6 +2,6 @@ defmodule Telephony.Core do
   alias Telephony.Core.Subscriber
 
   def create_subscriber(subscribers, payload) do
-    [Subscriber.new(payload) | subscribers]
+    subscribers ++ [Subscriber.new(payload)]
   end
 end
--- test/telephony/core_test.exs
+++ test/telephony/core_test.exs
@@ -3,6 +3,19 @@ defmodule Telephony.CoreTest do
 
   alias Telephony.{Core, Core.Subscriber}
 
+  setup do
+    subscribers = [
+      %Subscriber{
+        full_name: "John Doe",
+        id: "1234567890",
+        phone: "1234567890",
+        subscriber_type: :prepaid
+      }
+    ]
+
+    {:ok, subscribers: subscribers}
+  end
+
   describe "create_subscribers/2" do
     test "with valid params" do
       # Given
@@ -24,6 +37,37 @@ defmodule Telephony.CoreTest do
           full_name: "John Doe",
           id: "1234567890",
           phone: "1234567890",
+          subscriber_type: :prepaid
+        }
+      ]
+
+      assert expect == result
+    end
+
+    test "with valid params and existing subscribers", %{subscribers: subscribers} do
+      # Given
+      payload =
+        %{
+          full_name: "Jane Doe",
+          id: "0987654321",
+          phone: "0987654321"
+        }
+
+      # When
+      result = Core.create_subscriber(subscribers, payload)
+
+      # Then
+      expect = [
+        %Subscriber{
+          full_name: "John Doe",
+          id: "1234567890",
+          phone: "1234567890",
+          subscriber_type: :prepaid
+        },
+        %Subscriber{
+          full_name: "Jane Doe",
+          id: "0987654321",
+          phone: "0987654321",
           subscriber_type: :prepaid
         }
       ]
