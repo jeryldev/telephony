04a35470e2c44f71976e14d57d7ae1baef21fb96
jeryldev <jeryldev@gmail.com>
2024-01-02 22:21:36 +0800

    write a test and the core module for subscriber

---
+++ test/subscriber/subscriber_test.exs
@@ -0,0 +1,29 @@
+defmodule Subscriber.SubscriberTest do
+  use ExUnit.Case
+
+  alias Subscriber.Subscriber
+
+  describe "new/1" do
+    test "with valid payload" do
+      # Given
+      payload = %{
+        full_name: "John Doe",
+        id: "1234567890",
+        phone: "1234567890"
+      }
+
+      # When
+      result = Subscriber.new(payload)
+
+      # Then
+      expect = %Subscriber{
+        full_name: "John Doe",
+        id: "1234567890",
+        phone: "1234567890",
+        subscriber_type: :prepaid
+      }
+
+      assert expect == result
+    end
+  end
+end
