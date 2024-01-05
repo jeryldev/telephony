0213d2e4e2c3b23e67b37806edb9b5adb02d9da9
jeryldev <jeryldev@gmail.com>
2024-01-05 18:21:01 +0800

    create core file with a create_subscriber function

--- lib/telephony/core.ex
+++ lib/telephony/core.ex
@@ -1,7 +1,17 @@
 defmodule Telephony.Core do
   alias Telephony.Core.Subscriber
 
-  def create_subscriber(subscribers, payload) do
-    subscribers ++ [Subscriber.new(payload)]
+  @subscriber_types ~w(prepaid postpaid)a
+
+  def create_subscriber(subscribers, %{subscriber_type: subscriber_type} = payload)
+      when subscriber_type in @subscriber_types do
+    case Enum.find(subscribers, &(&1.phone == payload.phone)) do
+      nil -> subscribers ++ [Subscriber.new(payload)]
+      _ -> {:error, "Subscriber `#{payload.phone}`, already exists"}
+    end
+  end
+
+  def create_subscriber(_subscribers, _payload) do
+    {:error, "Only 'prepaid' and 'postpaid' are accepted"}
   end
 end
