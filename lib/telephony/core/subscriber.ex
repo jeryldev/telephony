73f22a84985a9196e08b609e9977172af6276354
jeryldev <jeryldev@gmail.com>
2024-01-05 18:40:13 +0800

    update the subsriber to reflect postpaid and prepaid structs depending on the payload subscriber type

--- lib/telephony/core/subscriber.ex
+++ lib/telephony/core/subscriber.ex
@@ -1,7 +1,15 @@
 defmodule Telephony.Core.Subscriber do
+  alias Telephony.Core.{Postpaid, Prepaid}
+
   defstruct [:full_name, :phone, subscriber_type: :prepaid]
 
-  def new(payload) do
+  def new(%{subscriber_type: :prepaid} = payload) do
+    payload = %{payload | subscriber_type: %Prepaid{}}
+    struct(__MODULE__, payload)
+  end
+
+  def new(%{subscriber_type: :postpaid} = payload) do
+    payload = %{payload | subscriber_type: %Postpaid{}}
     struct(__MODULE__, payload)
   end
 end
