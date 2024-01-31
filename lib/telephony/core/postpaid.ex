f5aca1c80f2c2d2ae85c0482e96de76836c7336c
jeryldev <jeryldev@gmail.com>
2024-01-31 11:09:12 +0800

    implement the make_call and make_recharge inside the Postpaid

--- lib/telephony/core/postpaid.ex
+++ lib/telephony/core/postpaid.ex
@@ -1,43 +1,45 @@
 defmodule Telephony.Core.Postpaid do
-  alias Telephony.Core.{Call, Invoice}
+  alias Telephony.Core.Call
 
   defstruct spent: 0
 
-  @price_per_minute 1.04
-
-  def make_call(subscriber, time_spent, date) do
-    subscriber
-    |> update_spent(time_spent)
-    |> add_call(time_spent, date)
-  end
-
-  def update_spent(%{subscriber_type: subscriber_type} = subscriber, time_spent) do
-    new_spent = @price_per_minute * time_spent
-    subscriber_type = %{subscriber_type | spent: subscriber_type.spent + new_spent}
-    %{subscriber | subscriber_type: subscriber_type}
-  end
-
-  def add_call(subscriber, time_spent, date) do
-    call = Call.new(time_spent, date)
-    %{subscriber | calls: subscriber.calls ++ [call]}
-  end
-
-  defimpl Invoice, for: __MODULE__ do
+  defimpl Subscriber, for: __MODULE__ do
     @price_per_minute 1.04
 
-    def print(_postpaid_subscriber_type, calls, year, month) do
-      Enum.reduce(calls, %{calls: [], value_spent: 0}, fn call, acc ->
-        if call.date.year == year and call.date.month == month do
-          call_map =
-            call
-            |> Map.take([:time_spent, :date])
-            |> Map.put(:value_spent, call.time_spent * @price_per_minute)
-
-          %{calls: acc.calls ++ [call_map], value_spent: acc.value_spent + call_map.value_spent}
-        else
-          acc
-        end
-      end)
+    def print_invoice(_subscriber_type, calls, year, month) do
+      Enum.reduce(calls, %{calls: [], value_spent: 0}, &filter_call(&1, &2, year, month))
     end
+
+    def make_call(subscriber_type, time_spent, date) do
+      subscriber_type
+      |> update_spent(time_spent)
+      |> add_call(time_spent, date)
+    end
+
+    def make_recharge(_subscriber_type, _value, _date) do
+      {:error, "Only prepaid can make a recharge"}
+    end
+
+    def update_spent(subscriber_type, time_spent) do
+      new_spent = @price_per_minute * time_spent
+      %{subscriber_type | spent: subscriber_type.spent + new_spent}
+    end
+
+    def add_call(subscriber_type, time_spent, date) do
+      call = Call.new(time_spent, date)
+      {subscriber_type, call}
+    end
+
+    defp filter_call(%{date: date} = call, acc, year, month)
+         when date.year == year and date.month == month do
+      call_map =
+        call
+        |> Map.take([:time_spent, :date])
+        |> Map.put(:value_spent, call.time_spent * @price_per_minute)
+
+      %{calls: acc.calls ++ [call_map], value_spent: acc.value_spent + call_map.value_spent}
+    end
+
+    defp filter_call(_call, acc, _year, _month), do: acc
   end
 end
